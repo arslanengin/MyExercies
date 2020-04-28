f6683969feab3cecb3352cf39367d2995f8f7f40
arslanengin <enginarslan48@gmail.com>
2020-04-29 01:22:55 +0300

    Add project files.

---
+++ Exercies/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ValueTypeReferenceType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sehirler1 = new string[] { "Mugla", "İstanbul" };
+            string[] sehirler2 = new string[] { "Mugla", "İstanbul" };
+            sehirler1 = sehirler2;
+            sehirler2[0] = "Aydın";
+            foreach (var item in sehirler1)
+            {
+                Console.WriteLine(item);
+            }
+
+            string a = "a1";
+            string b = "b1";
+            b = a;
+            a = "a2";
+            Console.WriteLine(b);
+
+            //String is a reference type that behaves like a value type
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        //classlara arayuz gorevı gorur classların o operasyonları ımzalamasını zorunlu hale getırır
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new BirinciMevzuat());
+            customerManager.Add();
+        }
+
+         class CustomerManager
+        {
+             void AddWithoutInterface(int mevzuat)
+            {
+                if(mevzuat == 1)
+                {
+                    Console.WriteLine("Musterı 1. mevzuata göre eklendi");
+                }
+                else
+                {
+                    Console.WriteLine("Musterı 2.mevzuata göre eklendi");
+                }
+            }
+
+            private  IMevzuat _mevzuat;
+            public CustomerManager(IMevzuat mevzuat)
+            {
+                _mevzuat = mevzuat;
+            }
+            public void Add()
+            {
+                _mevzuat.IslemYap();
+            }
+         }
+
+        interface IMevzuat
+        {
+            void IslemYap();
+        }
+
+        class BirinciMevzuat : IMevzuat
+        {
+            public void IslemYap()
+            {
+                Console.WriteLine("Birinci mevzuata göre işlem yapıldı");
+            }
+        }
+
+        class İkinciMevzuat : IMevzuat
+        {
+            public void IslemYap()
+            {
+                Console.WriteLine("İkinci mevzuata göre işlem yapıldı");
+            }
+        }
+    }
+}
